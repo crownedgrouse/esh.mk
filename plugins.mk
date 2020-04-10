4674f1f5ff8fe947e16ec11a2b7ecd94a678ae98
crownedgrouse <eric.pailleau@wanadoo.fr>
2020-04-10 21:25:29 +0200

    Add a simple test

--- plugins.mk
+++ plugins.mk
@@ -4,10 +4,13 @@
 #  Author : Eric Pailleau <esh.mk@crownegrouse.com>
 #  Version : 1.0.0
 
-help::
+help-plugins::
 	@printf "%s\n" "  esh            Apply esh template engine plugin"
 	@printf "%s\n" "  esh_help       Help of esh template engine"
 
 
 THIS := $(dir $(realpath $(lastword $(MAKEFILE_LIST))))
 include $(THIS)/mk/esh.mk
+
+test::
+	@$(MAKE) -f $(realpath $(lastword $(MAKEFILE_LIST))) esh && diff test/test1.ref test/test1
